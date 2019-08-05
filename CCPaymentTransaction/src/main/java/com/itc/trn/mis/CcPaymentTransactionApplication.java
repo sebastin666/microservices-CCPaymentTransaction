1df9a60a34441d296ebd7878706a26b165a91a4a
34807 <34807@BLRATIFWN34807.ITCINFOTECH.com>
2019-08-05 20:22:56 +0530

    3

--- CCPaymentTransaction/src/main/java/com/itc/trn/mis/CcPaymentTransactionApplication.java
+++ CCPaymentTransaction/src/main/java/com/itc/trn/mis/CcPaymentTransactionApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,9 +19,27 @@ public class CcPaymentTransactionApplication {
 	}
 	
 	@Bean
+	@Primary
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder getWebClicentBuilder() {
+		return WebClient.builder();
+	}
+	
+	//
+	
+	@Bean(name="restTemplate_TO")
+	@LoadBalanced
+	public RestTemplate getRestTemplate_TO() {
+		HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		httpComponentsClientHttpRequestFactory.setConnectTimeout(3000);
+		httpComponentsClientHttpRequestFactory.setReadTimeout(3000);
+		return new RestTemplate(httpComponentsClientHttpRequestFactory);
 	}
 
 }
--- CCPaymentTransaction/src/main/java/com/itc/trn/mis/controller/PaymentTransactionController.java
+++ CCPaymentTransaction/src/main/java/com/itc/trn/mis/controller/PaymentTransactionController.java
@@ -1,8 +1,11 @@
 package com.itc.trn.mis.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.itc.trn.mis.model.PaymentInfo;
 import com.itc.trn.mis.model.PaymentReceipt;
@@ -21,28 +26,27 @@ import com.itc.trn.mis.model.TransactionInfo;
 public class PaymentTransactionController {
 
 	private static final Logger logger = LoggerFactory.getLogger(PaymentTransactionController.class);
-	
+
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Qualifier("restTemplate_TO")
+	@Autowired
+	RestTemplate restTemplate_TO;
+
+	@Autowired
+	WebClient.Builder webClientBuilder;
+
 	@PostMapping(value = "/paymerchant", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> payMerchant(@RequestBody PaymentInfo paymentInfo){
+	public ResponseEntity<?> payMerchant(@RequestBody PaymentInfo paymentInfo) {
 		try {
-			logger.info("Payload::"+paymentInfo);
+			logger.info("Payload::" + paymentInfo);			
 			//
-			String approvalStatus = restTemplate.getForObject("http://CC-TransactionBankApproval/cardverification/transactionapproval/"+paymentInfo.getCardNo(), String.class);			
-			logger.info("Entry-approvalStatus::"+approvalStatus);
-			//			
-			TransactionInfo transactionInfo = restTemplate.postForObject("http://CC-PaymentGateway/paymentgateway/moneytransfer", paymentInfo, TransactionInfo.class);
-			logger.info("TransactionInfo==>"+transactionInfo);
+			PaymentReceipt paymentReceipt = callByRestTemplate(paymentInfo);
+//			PaymentReceipt paymentReceipt = callByWebClient(paymentInfo);
+			// Client side time out call
+//			PaymentReceipt paymentReceipt = callByRestTemplate_TO(paymentInfo);
 			//
-			PaymentReceipt paymentReceipt = restTemplate.postForObject("http://CC-PaymentReceipt/receipt/transreceipt", paymentInfo, PaymentReceipt.class);
-			//
-			paymentReceipt.setApprovalStatus(approvalStatus); 					// Update From Bank Approval Service
-			paymentReceipt.setTransId(transactionInfo.getTransId());			// Update From Payment Gateway Service
-			paymentReceipt.setTransStatus(transactionInfo.getTransStatus()); 	// Update From Payment Gateway Service
-			paymentReceipt.setTransTime(transactionInfo.getTransTime()); 		// Update From Payment Gateway Service
-			//			
 			return new ResponseEntity<PaymentReceipt>(paymentReceipt, HttpStatus.OK);
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -50,4 +54,90 @@ public class PaymentTransactionController {
 			return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
+
+	private PaymentReceipt callByRestTemplate(PaymentInfo paymentInfo) {
+		logger.info("************ callByRestTemplate *************");
+		logger.info("paymentInfo.getCardNo===>::" + paymentInfo.getCardNo());
+		String approvalStatus = restTemplate.getForObject(
+				"http://CC-TransactionBankApproval/cardverification/transactionapproval/" + paymentInfo.getCardNo(),
+				String.class);
+		logger.info("Entry-approvalStatus::" + approvalStatus);
+		//
+		TransactionInfo transactionInfo = restTemplate.postForObject(
+				"http://CC-PaymentGateway/paymentgateway/moneytransfer", paymentInfo, TransactionInfo.class);
+		logger.info("TransactionInfo==>" + transactionInfo);
+		//
+		PaymentReceipt paymentReceipt = restTemplate.postForObject("http://CC-PaymentReceipt/receipt/transreceipt",
+				paymentInfo, PaymentReceipt.class);
+		//
+		paymentReceipt.setApprovalStatus(approvalStatus); // Update From Bank Approval Service
+		paymentReceipt.setTransId(transactionInfo.getTransId()); // Update From Payment Gateway Service
+		paymentReceipt.setTransStatus(transactionInfo.getTransStatus()); // Update From Payment Gateway Service
+		paymentReceipt.setTransTime(transactionInfo.getTransTime()); // Update From Payment Gateway Service
+		//
+		return paymentReceipt;
+	}
+
+	private PaymentReceipt callByWebClient(PaymentInfo paymentInfo) {
+		logger.info("************ callByWebClient *************");
+		//
+		String approvalStatus = webClientBuilder.build().get()
+				.uri("http://CC-TransactionBankApproval/cardverification/transactionapproval/" + paymentInfo.getCardNo())
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+		logger.info("Entry-approvalStatus::" + approvalStatus);
+		//
+		TransactionInfo transactionInfo  = webClientBuilder.build().post()
+				.uri("http://CC-PaymentGateway/paymentgateway/moneytransfer")
+				.body(BodyInserters.fromObject(paymentInfo))
+				.retrieve()
+				.bodyToMono(TransactionInfo.class)
+				.block();
+		logger.info("TransactionInfo==>" + transactionInfo);
+		//
+		PaymentReceipt paymentReceipt = webClientBuilder.build().post()
+				.uri("http://CC-PaymentReceipt/receipt/transreceipt")
+				.body(BodyInserters.fromObject(paymentInfo))
+				.retrieve()
+				.bodyToMono(PaymentReceipt.class)
+				.block();
+		//
+		paymentReceipt.setApprovalStatus(approvalStatus); // Update From Bank Approval Service
+		paymentReceipt.setTransId(transactionInfo.getTransId()); // Update From Payment Gateway Service
+		paymentReceipt.setTransStatus(transactionInfo.getTransStatus()); // Update From Payment Gateway Service
+		paymentReceipt.setTransTime(transactionInfo.getTransTime()); // Update From Payment Gateway Service
+		//
+		return paymentReceipt;
+	}
+	
+	private PaymentReceipt callByRestTemplate_TO(PaymentInfo paymentInfo) {
+		logger.info("************ callByRestTemplate_TO *************");
+		logger.info("Time Before Call===>::" + new Date());
+		String approvalStatus = "REJ";
+		try {
+		 approvalStatus = restTemplate_TO.getForObject(
+				"http://CC-TransactionBankApproval/cardverification/transactionapproval/" + paymentInfo.getCardNo(),
+				String.class);
+		}catch(Exception ex) {
+			logger.info("Time out on the conection");
+		}
+		logger.info("Time After Call===>::" + new Date());
+		logger.info("Entry-approvalStatus::" + approvalStatus);
+		//
+		TransactionInfo transactionInfo = restTemplate_TO.postForObject(
+				"http://CC-PaymentGateway/paymentgateway/moneytransfer", paymentInfo, TransactionInfo.class);
+		logger.info("TransactionInfo==>" + transactionInfo);
+		//
+		PaymentReceipt paymentReceipt = restTemplate_TO.postForObject("http://CC-PaymentReceipt/receipt/transreceipt",
+				paymentInfo, PaymentReceipt.class);
+		//
+		paymentReceipt.setApprovalStatus(approvalStatus); // Update From Bank Approval Service
+		paymentReceipt.setTransId(transactionInfo.getTransId()); // Update From Payment Gateway Service
+		paymentReceipt.setTransStatus(transactionInfo.getTransStatus()); // Update From Payment Gateway Service
+		paymentReceipt.setTransTime(transactionInfo.getTransTime()); // Update From Payment Gateway Service
+		//
+		return paymentReceipt;
+	}
+
 }
